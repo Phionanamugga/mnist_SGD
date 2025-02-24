178ec6ada0acc0c746e21ee4daa8ab4fb049ccfa
Phionanamugga <phionanamugga15@gmail.com>
2025-02-24 19:16:00 +0100

    imports libraries for python script sgd_variants[Delivers#188920450]

---
+++ src/sgd_variants.py
@@ -0,0 +1,7 @@
+# 📌 SGD Variants: Mini-Batch & Full-Batch Gradient Descent
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error

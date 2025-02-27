80c7385ef7020e89b703af0fd8eb25b52e4bdc2d
Phionanamugga <phionanamugga15@gmail.com>
2025-02-27 20:38:34 +0100

    implements ADAM[Delivers#188920454]

--- src/optimizers.py
+++ src/optimizers.py
@@ -72,4 +72,29 @@ class RMSProp:
             self.cache[i] = self.decay_rate * self.cache[i] + (1 - self.decay_rate) * grads[i] ** 2
             params[i] -= (self.lr / (np.sqrt(self.cache[i]) + self.epsilon)) * grads[i]
 
+class Adam:
+    """Adam Optimizer"""
+    def __init__(self, learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.lr = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = None
+        self.v = None
+        self.t = 0
+
+    def update(self, params, grads):
+        if self.m is None:
+            self.m = [np.zeros_like(p) for p in params]
+            self.v = [np.zeros_like(p) for p in params]
+
+        self.t += 1
+        for i in range(len(params)):
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grads[i]
+            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (grads[i] ** 2)
+
+            m_hat = self.m[i] / (1 - self.beta1 ** self.t)
+            v_hat = self.v[i] / (1 - self.beta2 ** self.t)
+
+            params[i] -= (self.lr / (np.sqrt(v_hat) + self.epsilon)) * m_hat
 

c76912116cae11dd9be4ef6df98b11d0d92b7eee
Phionanamugga <phionanamugga15@gmail.com>
2025-02-27 20:33:41 +0100

    implements SGD momentum[Delivers#188920454]

--- src/optimizers.py
+++ src/optimizers.py
@@ -9,3 +9,20 @@ class SGD:
     def update(self, params, grads):
         for i in range(len(params)):
             params[i] -= self.lr * grads[i]
+
+class SGDMomentum:
+    """SGD with Momentum"""
+    def __init__(self, learning_rate=0.01, momentum=0.9):
+        self.lr = learning_rate
+        self.momentum = momentum
+        self.velocity = None
+
+    def update(self, params, grads):
+        if self.velocity is None:
+            self.velocity = [np.zeros_like(p) for p in params]
+
+        for i in range(len(params)):
+            self.velocity[i] = self.momentum * self.velocity[i] - self.lr * grads[i]
+            params[i] += self.velocity[i]
+
+            

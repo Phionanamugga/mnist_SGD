afd2693a3cfaf2002c23e82a7ab6ded72c62d1f9
Phionanamugga <phionanamugga15@gmail.com>
2025-02-27 20:32:13 +0100

    implements vanilla SGD[Delivers#188920454]

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,2 +1,11 @@
 # 📌 Implementing Optimizers from Scratch
 import numpy as np
+
+class SGD:
+    """Vanilla Stochastic Gradient Descent"""
+    def __init__(self, learning_rate=0.01):
+        self.lr = learning_rate
+
+    def update(self, params, grads):
+        for i in range(len(params)):
+            params[i] -= self.lr * grads[i]

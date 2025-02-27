206bf1e657a4e7faec0485ea0141a545e89222f3
Phionanamugga <phionanamugga15@gmail.com>
2025-02-27 20:45:16 +0100

    loads dataset[Delivers#188920451]

--- src/learning_rate_schedulers.py
+++ src/learning_rate_schedulers.py
@@ -6,3 +6,11 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+
+# ✅ Load MNIST Dataset
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+# ✅ Define Neural Network
